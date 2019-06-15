114b0a44cf43e9d1dc1e27c3f7bdc62a3ff289df
Seung Jin Jang <secky2@hanmail.net>
2019-06-15 16:57:34 +0900

    Added main function & parse arguments

--- videocapture.py
+++ videocapture.py
@@ -1,22 +1,46 @@
+import argparse
+import datetime
+import os
+import sys
+
 import cv2
 
-cap = cv2.VideoCapture(0)
-if cap.isOpened:
-    print('{0} x {1}'.format(cap.get(3), cap.get(4)))
 
-while True:
-    ret, fram = cap.read()
-    
-    if ret:
-        image = cv2.cvtColor(fram, cv2.COLOR_RGB2BGR)
-        cv2.imshow('image', image)
+def do_main(dir_out):
+    cap = cv2.VideoCapture(0)
+    if cap.isOpened:
+        print('{0} x {1}'.format(cap.get(3), cap.get(4)))
+
+    while True:
+        ret, fram = cap.read()
         
-        k = cv2.waitKey(100) & 0xFF
-        if k == 27:
-            break
-        elif k == ord('s'):
-            cv2.imwrite('cap.png', image)
-    else:
-        print('error')
+        if ret:
+            image = cv2.cvtColor(fram, cv2.COLOR_RGB2BGR)
+            cv2.imshow('image', image)
+            
+            k = cv2.waitKey(100) & 0xFF
+            if k == 27:
+                break
+            elif k == ord('s'):
+                now = datetime.datetime.now()
+                filename = now.strftime('%Y-%m-%d_%H-%M-%S')
+                cv2.imwrite(os.path.join(dir_out, filename + '.png'), image)
+        else:
+            print('error')
 
-cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    # parsing arguments
+    parser = argparse.ArgumentParser(description='Command line argument')
+    parser.add_argument('-o', '--output-directory', type=str,
+                        default='img-captured', help='specify directory to save captured images')
+    args = parser.parse_args()
+
+    # check and initialize
+    if not os.path.isdir(args.output_directory):
+        os.mkdir(args.output_directory)
+
+    # run main job for image cropping
+    do_main(args.output_directory)
