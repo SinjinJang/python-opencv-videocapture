f211f9fabb398a500082d8e39fefde41d0510570
Seung Jin Jang <secky2@hanmail.net>
2019-06-16 21:46:56 +0900

    Changed the key to quit the program

--- videocapture.py
+++ videocapture.py
@@ -34,7 +34,7 @@ def do_main(dev_id, dir_out, cap_sz):
             cv2.imshow('image', image)
             
             k = cv2.waitKey(100) & 0xFF
-            if k == 27:
+            if k == ord('q'):
                 break
             elif k == ord('s'):
                 now = datetime.datetime.now()
